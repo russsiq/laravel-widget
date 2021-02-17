3e210ce6238b9083c5d66a806e2dad10db1de020
russsiq <rusiq@list.ru>
2021-02-18 03:32:03 +1000

    Create Parameters.php

---
+++ src/Support/Parameters.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Russsiq\Widget\Support;
+
+use Russsiq\Widget\Contracts\ParameterBagContract;
+use Symfony\Component\HttpFoundation\ParameterBag;
+
+class Parameters extends ParameterBag implements ParameterBagContract
+{
+}
